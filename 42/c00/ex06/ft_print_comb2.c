39a7b58240a1f3eaf01969b8701661c6b8f562ae
Andranik Karapetyan <andrkara@c2r1s2.42yerevan.am>
2025-10-14 22:35:05 +0400

    verjapes tany

--- 42/c00/ex06/ft_print_comb2.c
+++ 42/c00/ex06/ft_print_comb2.c
@@ -1,23 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrkara <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 23:11:56 by andrkara          #+#    #+#             */
+/*   Updated: 2025/10/14 22:33:45 by andrkara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
-void ft_print_comb2(void){
-	int a=0;
-	int b=0;
 
-	while (a<=10){
-		b=a+1;
-		while (b<=11){
-			write(1, &a, 1);
-			write(1, &b, 1);
-			write (1, ", ", 2);
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_writenumcomb(int a, int b)
+{
+	ft_putchar('0' + (a/10));
+	ft_putchar('0' + (a%10)); 
+}
+
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
+
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			;
+			2++;
+					}
+				}
+
+
+				if (a1 != '9' && a2 != '8')
+					write(1, ", ", 2);
+				else
+					write(1, "\n", 1);
+				}
+			}
 			b++;
 		}
 		a++;
 	}
-	write (1, "\n", 1);
-}
-
-int main(){
-	ft_print_comb2();
-	return 0;
 }
