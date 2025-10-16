5be08d7eefcbdc82713bd1ae487fd2891c5ea27e
Andranik Karapetyan <andrkara@c2r12s4.42yerevan.am>
2025-10-16 23:02:15 +0400

    ex00 done

--- 42/c01/ex00/ft_ft.c
+++ 42/c01/ex00/ft_ft.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrkara <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 21:54:38 by andrkara          #+#    #+#             */
+/*   Updated: 2025/10/16 23:01:43 by andrkara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void    ft_putchar(char c)
@@ -5,35 +17,7 @@ void    ft_putchar(char c)
         write (1, &c, 1);
 }
 
-void ft_putnbr(int nb)
-{
-        if (nb == -2147483648)
-        {
-        write(1, "-2147483648", 11);
-        return;
-        }
-        if (nb < 0)
-        {
-        ft_putchar('-');
-        nb = -nb;
-        }
-        if (nb > 9)
-                ft_putnbr(nb / 10);
-        ft_putchar(nb % 10 + '0');
-}
-
 void	ft_ft(int *nbr)
 {
-	nbr = 42; 
-}
-
-int	main()
-{
-	int a;
-	int *b;
-
-	a = 96;
-	ft_ft(a);
-	ft_putnbr(b);
-	return 0;	
+	*nbr = 42; 
 }
